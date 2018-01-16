2ccfa943699a22aa6194d3bc4c612c8f81903fe5
saumya <theiosdevguy@gmail.com>
2018-01-16 10:30:57 +0530

    Include fabric

--- src/app/components/formitems/CustomCanvas.js
+++ src/app/components/formitems/CustomCanvas.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
+require('fabric');
 
 class CustomCanvas extends Component {
   polygonMode = true;

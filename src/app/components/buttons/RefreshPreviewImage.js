0a63e26f1af1ce25a6a177d3bb7b8299acfbb81a
Marc Tanne <tannem9@gmail.com>
2018-03-12 18:27:14 -0700

    added clear statement to timeout function

--- src/app/components/buttons/RefreshPreviewImage.js
+++ src/app/components/buttons/RefreshPreviewImage.js
@@ -27,7 +27,7 @@ class RefreshPreviewImage extends Component {
         flagTimeout = setTimeout(() => {
           if (nextProps.imageUpdateInProgress && this.props.data.imageUpdateInProgress === nextProps.imageUpdateInProgress){
             if(this.state.disabledFlag){
-              message.error('Timeout for fetching image. Please try again later.');
+              message.error('Timeout for fetching image.');
               this.setState({disabledFlag: false});
               clearTimeout(flagTimeout);
             } else {

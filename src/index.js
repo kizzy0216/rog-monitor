9571c17cd025156ad58a92e91fd92bd2291b492a
Marc Tanne <tannem9@gmail.com>
2018-04-26 19:37:11 -0700

    changed conditional to match dev url and added updated yarn.lock file.

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import store from './app/redux/store';
 import App from './app/App.js';
 import App401 from './app/App401.js';
 import registerServiceWorker from './registerServiceWorker';
-
-if (process.env.NODE_ENV == 'production') {
+console.log(process.env.PUBLIC_URL);
+if (process.env.PUBLIC_URL == 'https://rog-monitor-dev.herokuapp.com') {
   var credentials = window.prompt("Enter Realm Password");
   if (!credentials|| credentials !== 'GoRogTeam!') {
     render401();

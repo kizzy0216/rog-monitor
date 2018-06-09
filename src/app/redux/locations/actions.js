e63fce988cbc987f1893f4f20b5d246f1046751a
Marc Tanne <tannem9@gmail.com>
2018-06-08 17:06:46 -0700

    removed default value from function

--- src/app/redux/locations/actions.js
+++ src/app/redux/locations/actions.js
@@ -227,7 +227,7 @@ export function fetchLocations(user) {
   }
 }
 
-export function addLocationCamera(user, location, name, rtspUrl, username = "admin", password) {
+export function addLocationCamera(user, location, name, rtspUrl, username, password) {
   return (dispatch) => {
     dispatch(addLocationCameraError(''));
     dispatch(addLocationCameraInProcess(true));

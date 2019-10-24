3046611d693f9a6f8f43e9984c5d081b3b143e56
Marc Tanne <tannem9@gmail.com>
2019-10-24 11:14:26 -0700

    forced to lowercase so postgres will store case insensitive email field in all lowercase for later matching

--- src/app/components/modals/JoinRogModal.js
+++ src/app/components/modals/JoinRogModal.js
@@ -65,7 +65,7 @@ class RequestInviteModal extends Component {
         return;
       }
 
-      this.props.sendInvitationEmail(values.email);
+      this.props.sendInvitationEmail(values.email.toLowerCase());
     });
   };
 

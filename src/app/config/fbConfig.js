6b786edd88cb632a44ddf8ee36e78f0aa911f6d0
Marc Tanne <tannem9@gmail.com>
2019-12-26 15:42:50 -0700

    fix for: Ghost Alerts from other accounts bug.

--- src/app/config/fbConfig.js
+++ src/app/config/fbConfig.js
@@ -3,6 +3,9 @@ import 'firebase/messaging'
 
 // TODO: move this to env config somehow
 var config = {
+  projectId: "rog-2-0",
+  apiKey: "AIzaSyCY1oTVrozQfDrCG1k-b3Q4Iw5iWSF_LIM",
+  appId: "1:153344187169:web:64dd90de0f9831cc643c60",
   messagingSenderId: "153344187169"
 };
 

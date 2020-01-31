e94675e05af8a891e9029c266a68962f88e15541
Marc Tanne <tannem9@gmail.com>
2020-01-31 14:54:11 -0800

    fixed function call arguments for fetch userCameraLicenses

--- src/app/components/cameras/CameraOptionButtons.js
+++ src/app/components/cameras/CameraOptionButtons.js
@@ -30,7 +30,7 @@ class CameraOptionButtons extends Component {
   }
 
   countAvailableCameraLicenses = () => {
-    this.props.fetchUserCameraLicenses(this.props.user, this.props.cameraGroups)
+    this.props.fetchUserCameraLicenses(this.props.user)
     let count = 0;
     this.props.user.cameraLicenses.map(cameraLicense => cameraLicense.cameras_uuid == null ? count++ : count)
     return count;
@@ -120,7 +120,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    fetchUserCameraLicenses: (user, cameraGroup) => dispatch(fetchUserCameraLicenses(user, cameraGroup))
+    fetchUserCameraLicenses: (user) => dispatch(fetchUserCameraLicenses(user))
   }
 };
 

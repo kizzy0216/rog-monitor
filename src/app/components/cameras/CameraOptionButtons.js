ce73e9ed8fbcf3c203692ab5f86473b5db31610d
Marc Tanne <tannem9@gmail.com>
2020-04-27 17:08:20 -0700

    changed help@gorog.co to hello@gorog.co

--- src/app/components/cameras/CameraOptionButtons.js
+++ src/app/components/cameras/CameraOptionButtons.js
@@ -26,7 +26,7 @@ class CameraOptionButtons extends Component {
     } else if (this.state.addCameraModalVisible === true) {
       this.setState({addCameraModalVisible: false})
     } else {
-      message.error("You have reached your license limit. Please send an email requesting additional licenses to help@gorog.co");
+      message.error("You have reached your license limit. Please send an email requesting additional licenses to hello@gorog.co");
     }
   }
 

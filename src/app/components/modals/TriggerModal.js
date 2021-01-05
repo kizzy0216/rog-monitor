cfdf6a1286ee128ee3710dfef6aab02ca6a62af8
kizzy0216 <m.kizzy.up@gmail.com>
2021-01-05 14:50:02 -0600

    Time Slider Fixed on Loitering

--- src/app/components/modals/TriggerModal.js
+++ src/app/components/modals/TriggerModal.js
@@ -105,7 +105,7 @@ const AddTriggerForm = ({
               setTriggerTimeWindows={setTriggerTimeWindows}
             />
           }
-          {canvasMode && (currentTriggerDetails.currentBaseTriggerType === 'LD') &&
+          {canvasMode && (currentTriggerDetails.currentBaseTriggerType === 'LD') && saveCancel &&
             <Row>
               <Col span={4} style={styles.LDtimeLeft}>
                 {convertToMilitaryFormat(loiteringSeconds)}
@@ -207,6 +207,7 @@ const AddTriggerForm = ({
                       style={{margin: '0 auto'}}
                       onChange={updateDataStart}
                       onOpenChange={(open: false), checkForWindow}
+                      // onOpenChange={checkForWindow}
                       allowClear={true}
                       placeholder="Start Time"
                       format={'HH:mm'}
@@ -217,7 +218,8 @@ const AddTriggerForm = ({
                       span={8}
                       style={{margin: '0 auto'}}
                       onChange={updateDataStop}
-                      onOpenChange={(open: false), checkForWindow}
+                      // onOpenChange={(open: false), checkForWindow}
+                      onOpenChange={checkForWindow}
                       allowClear={true}
                       placeholder="End Time"
                       format={'HH:mm'}
@@ -236,14 +238,14 @@ const AddTriggerForm = ({
                   </Form.Item>
                 </Row>
                 <Row>
-                <Col span={8}>
-                  <Button type="danger" icon={<DeleteOutlined />} onClick={deleteTriggerTimeWindow}>Delete Disarm Window</Button>
-                </Col>
+                  <Col span={8}>
+                    <Button type="danger" icon={<DeleteOutlined />} onClick={deleteTriggerTimeWindow}>Delete Disarm Window</Button>
+                  </Col>
                   <Col span={8}>
                     <Button type="danger" icon={<CloseOutlined />} onClick={resetData}>Clear Form</Button>
                   </Col>
                   <Col span={8}>
-                    <Button type="primary" icon=<SaveOutlined /> onClick={saveData}>Save Disarm Window</Button>
+                    <Button type="primary" icon={<SaveOutlined />} onClick={saveData}>Save Disarm Window</Button>
                   </Col>
                 </Row>
                 <div>&nbsp;</div>
@@ -365,7 +367,8 @@ const AddTriggerForm = ({
                         span={8}
                         style={{margin: '0 auto'}}
                         onChange={updateDataStart}
-                        onOpenChange={(open: false), checkForWindow}
+                        // onOpenChange={(open: false), checkForWindow}
+                        onOpenChange={checkForWindow}
                         allowClear={true}
                         placeholder="Start Time"
                         format={'HH:mm'}
@@ -376,7 +379,8 @@ const AddTriggerForm = ({
                         span={8}
                         style={{margin: '0 auto'}}
                         onChange={updateDataStop}
-                        onOpenChange={(open: false), checkForWindow}
+                        // onOpenChange={(open: false), checkForWindow}
+                        onOpenChange={checkForWindow}
                         allowClear={true}
                         placeholder="End Time"
                         format={'HH:mm'}
@@ -419,9 +423,9 @@ class AddTriggerModal extends Component {
   constructor(props) {
     super(props);
     let rogProtect = true;
-    if (typeof props.data.s3_keywords !== 'undefined' && props.data.s3_keywords !== null && props.data.s3_keywords.hasOwnProperty('length')) {
-      let rogProtect = props.data.s3_keywords.length === 0 ? true : false
-    }
+    // if (typeof props.data.s3_keywords !== 'undefined' && props.data.s3_keywords !== null && props.data.s3_keywords.hasOwnProperty('length')) {
+    //   rogProtect = props.data.s3_keywords.length === 0 ? true : false
+    // }
     this.state = {
       visible: false,
       error: false,
@@ -452,13 +456,13 @@ class AddTriggerModal extends Component {
   }
 
   UNSAFE_componentWillReceiveProps(nextProps) {
-    if (typeof nextProps.data.s3_keywords !== 'undefined' && nextProps.data.s3_keywords !== null && nextProps.data.s3_keywords.hasOwnProperty('length')) {
-      if (nextProps.data.s3_keywords.length === 0) {
-        this.setState({rogProtect: true});
-      } else {
-        this.setState({rogProtect: false});
-      }
-    }
+    // if (typeof nextProps.data.s3_keywords !== 'undefined' && nextProps.data.s3_keywords !== null && nextProps.data.s3_keywords.hasOwnProperty('length')) {
+    //   if (nextProps.data.s3_keywords.length === 0) {
+    //     this.setState({rogProtect: true});
+    //   } else {
+    //     this.setState({rogProtect: false});
+    //   }
+    // }
     if (this.props.polygonData !== undefined && !isEmpty(this.props.polygonData)) {
       if (nextProps.polygonData !== undefined && !isEmpty(nextProps.polygonData) && this.props.polygonData !== nextProps.polygonData) {
         this.canvasKey++;

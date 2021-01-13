fd9179da273e023dd585b2057aa334fbd23bf787
kizzy0216 <m.kizzy.up@gmail.com>
2021-01-13 13:23:08 -0600

    Change back redux store

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -3,11 +3,10 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import { getFirebase } from 'react-redux-firebase';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(
+  compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase}))
   )
 );
